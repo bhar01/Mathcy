eb389ff8814850eee25f74203c5a4bb1ba3a6129
bhar01 <45250465+bhar01@users.noreply.github.com>
2019-02-08 19:27:08 +0530

    A webbased application that makes learning maths easier that make

---
+++ js/js-1.js
@@ -0,0 +1,7 @@
+function fun() {
+    	var a = parseInt(document.getElementById('a').value);
+    	var b = parseInt(document.getElementById('b').value);
+    	
+    	document.getElementById('s1').value = ("a + b");
+    	
+}
